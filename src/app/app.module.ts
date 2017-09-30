e7eaf9e7bed4d693a4f80b2a151da0d2f416c131
Konstantin Pozhidaev <kos89jed@gmal.com>
2017-09-30 18:57:57 +0300

    Componentize menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    MenuComponent
+  ]
 })
 export class AppModule { }
